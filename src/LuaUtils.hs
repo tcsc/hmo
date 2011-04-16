f238ce9756e8d6db8f69a11e217d16f43a4ea18e
Trent Clarke <1959769+tcsc@users.noreply.github.com>
2011-04-16 05:49:13 +0000

    - Added cutoff value to RTSP configuration. - Added some automatic stack cleanup actions to the LuaUtils library

--- src/LuaUtils.hs
+++ src/LuaUtils.hs
@@ -1,26 +1,37 @@
-module LuaUtils(mapTable) where
+module LuaUtils(mapTable, bracketField, bracketGlobal, bracketStack) where
   
-import qualified Scripting.Lua as Lua
-  
+import Scripting.Lua
+import Control.Exception
+
 -- | Maps the values of a Lua table into a list using the supplied function.
 --   Assumes that the table of interest is on the top of the Lua Interface 
 --   stack
-mapTable :: Lua.StackValue v => Lua.LuaState -> (v -> b) -> IO [b]
+mapTable :: StackValue v => LuaState -> (v -> b) -> IO [b]
 mapTable lua f = do 
-    isTable <- Lua.istable lua (-1)
+    isTable <- istable lua (-1)
     case isTable of
       False -> return []
-      True -> do { Lua.pushnil lua; traverse lua f []; }
+      True -> do { pushnil lua; traverse lua f []; }
   where 
     -- Assumes that the table of interest is at the top of the stack
-    traverse :: Lua.StackValue a => Lua.LuaState -> (a -> b) -> [b] -> IO [b]
+    traverse :: StackValue a => LuaState -> (a -> b) -> [b] -> IO [b]
     traverse l fn acc = do
-      x <- Lua.next l (-2) -- leaves the key at -2 and the value at -1
+      x <- next l (-2) -- leaves the key at -2 and the value at -1
       case x of 
         False -> return (reverse acc)
-        True -> do mval <- Lua.peek l (-1) 
+        True -> do mval <- peek l (-1) 
                    case mval of                         
                      Nothing -> return [] 
                      Just val -> do let acc' = (fn val) : acc
-                                    Lua.pop l 1
+                                    pop l 1
                                     traverse l fn acc'
+
+bracketStack :: LuaState -> (LuaState -> IO()) -> (LuaState -> IO a) -> IO a
+bracketStack lua open fn = bracket_ (open lua) (pop lua 1) (fn lua)
+
+bracketField :: LuaState -> String -> (LuaState -> IO a) -> IO a
+bracketField lua name fn = bracketStack lua (\l -> getfield l (-1) name) fn
+                                         
+bracketGlobal :: LuaState -> String -> (LuaState -> IO a) -> IO a
+bracketGlobal lua name action = bracketStack lua (\l -> getglobal l name) action
+          
