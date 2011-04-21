f077bbbe96257902b3c40a4958b5a41067064187
Trent Clarke <trent.clarke@gmail.com>
2011-04-21 10:29:52 +0000

    Added unit tests

--- src/Rtsp.hs
+++ src/Rtsp.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
+
 module Rtsp (
   Message (..),
   Packet (..),
@@ -8,18 +9,25 @@ module Rtsp (
   embeddedPacket,
   extractMessageBytes,
   parseMessage,
+  formatMessage,
+  formatPacket,
   msgSequence,
   msgHeaders,
-  msgContentLength
+  msgContentLength,
+  unitTests
 ) where
 
 import Data.Binary.Strict.Get
+import Data.Binary.Put
 import Data.Char
 import Data.List
+import Data.Maybe
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.UTF8 as Utf8
 import Data.ByteString.Search
 import Network.URI
+import Test.HUnit
 import Text.Parsec
 import Text.Parsec.Char
 
@@ -40,32 +48,32 @@ data Verb = Describe | Announce | Setup | Play | Teardown
 
 type Version = (Int, Int)
 
-data Message = Request Int Verb URI Version Headers Int
+data Message = Request Int Verb URI Version Headers
              | Response Int Status Headers
              
 data Packet = Packet Int B.ByteString deriving (Show)
   
 instance Show Message where 
-  show (Request s v u _ _ _) = "#" ++ (show s) ++ " " ++ (show v) ++ " " ++ (show u) 
+  show (Request s v u _ _) = "#" ++ (show s) ++ " " ++ (show v) ++ " " ++ (show u) 
   show (Response _ s _) = (show s) 
 
 -- | gets the sequence number of a message
 msgSequence :: Message -> Int
-msgSequence (Request s _ _ _ _ _) = s 
+msgSequence (Request s _ _ _ _ ) = s 
 msgSequence (Response s _ _) = s 
 
 msgHeaders :: Message -> Headers
-msgHeaders (Request _ _ _ _ hs _) = hs 
+msgHeaders (Request _ _ _ _ hs) = hs 
 msgHeaders (Response _ _ hs) = hs 
 
 msgContentLength :: Message -> Int
-msgContentLength (Request _ _ _ _ _ l) = l 
+msgContentLength (Request _ _ _ _ hs) = maybe 0 id (contentLength hs)
 msgContentLength (Response _ _ _) = 0 
 
 -- | Fills in the missing bits of a half-parsed message once we know what they are
-msgUpdate :: Int -> Headers -> Int -> Message -> Message
-msgUpdate s headers cLength (Request _ verb uri ver _ _) = Request s verb uri ver headers cLength
-msgUpdate s headers _ (Response _ status _) = Response s status headers  
+msgUpdate :: Int -> Headers -> Message -> Message
+msgUpdate s headers (Request _ verb uri ver _) = Request s verb uri ver headers
+msgUpdate s headers (Response _ status _) = Response s status headers  
 
 parseMessage :: B.ByteString -> Maybe Message
 parseMessage bytes = case parse message "" bytes of
@@ -79,20 +87,17 @@ message = do
     sq <- case readSequence h of 
             Just n -> return n
             Nothing -> fail "Missing sequence number"
-    let cLength = case (readContentLength h) of 
-                    Nothing -> 0
-                    Just n -> n
-    return $ msgUpdate sq h cLength msg
+    return $ msgUpdate sq h msg
   where
     readSequence :: Headers -> Maybe Int
     readSequence hs = do 
       s <- getHeader "cseq" hs
       maybeInt s
       
-    readContentLength :: Headers -> Maybe Int
-    readContentLength hs = case (getHeader "content-length" hs) of
-                             Nothing -> Nothing
-                             Just txt -> maybeInt txt
+contentLength :: Headers -> Maybe Int
+contentLength hs = case (getHeader "content-length" hs) of
+                     Nothing -> Nothing
+                     Just txt -> maybeInt txt
       
 request = do 
   v <- verb
@@ -100,7 +105,7 @@ request = do
   u <- uri
   spaces
   ver <- version
-  return $ Request (-1) v u ver empty 0
+  return $ Request (-1) v u ver empty
 
 response = do
   ver <- version
@@ -179,9 +184,19 @@ embeddedPacket bytes = case runGet getPacket bytes of
 extractMessageBytes :: B.ByteString -> Maybe (B.ByteString, B.ByteString)
 extractMessageBytes s =
   let (h,t) = breakAfter endOfMessage s
-  in case (B.isSuffixOf h endOfMessage) of
+  in case (endOfMessage `B.isSuffixOf` h) of
     True -> Just (h, t)
     False -> Nothing
+  
+formatMessage :: Message -> Maybe B.ByteString -> B.ByteString
+formatMessage (Request s v uri ver hs) _ = B.empty
+
+formatPacket :: Packet -> B.ByteString
+formatPacket (Packet channel bytes) = (B.concat . L.toChunks) $ runPut $ do 
+  putWord8 (fromIntegral 0x24)
+  putWord8 (fromIntegral channel)
+  putWord16be (fromIntegral (B.length bytes))
+  putByteString bytes
   
 endOfMessage :: B.ByteString 
 endOfMessage = B.pack [13, 10, 13, 10] 
@@ -197,3 +212,55 @@ fromEnum s = case s of
                OK -> 200
                NotFound -> 404
                InternalServerError -> 500
+
+-- ----------------------------------------------------------------------------
+-- Static data
+-- ----------------------------------------------------------------------------
+
+hdrSequence = "CSeq"
+hdrContentLength = "Content-Length"
+ 
+-- ----------------------------------------------------------------------------
+-- Unit tests
+-- ----------------------------------------------------------------------------
+
+testParseMinimalRequest = TestCase(do 
+  let bytes = Utf8.fromString "DESCRIBE rtsp://localhost/root/1 RTSP/1.0\r\nCSeq: 1\r\n\r\n"
+  let mmsg = parseMessage bytes
+  assertBool "Parse Success" (isJust mmsg)
+  let (Request sq verb uri ver hdr) = fromJust mmsg
+  assertEqual "sequence" 1 sq
+  assertEqual "verb" Describe verb
+  assertEqual "version" (1,0) ver
+  assertEqual "contentLength" 0 (maybe 0 (id) (contentLength hdr)))
+
+testParsePacket = TestCase (do
+  let bytes = B.pack [0x24, 0x01, 0x00, 0x05, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]
+  let mp = embeddedPacket bytes
+  assertBool "Parse failed" (isJust mp)
+  let ((Packet channel payload), remainder) = fromJust mp
+  assertEqual "channel" channel 1
+  assertEqual "payload size" 5 (B.length payload)
+  assertEqual "remainder size" 2 (B.length remainder))
+  
+testParsePacketNotEnoughData = TestCase (do
+    let bytes = B.pack [0x24, 0x01, 0x00, 0x10, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]
+    let mp = embeddedPacket bytes
+    assertBool "Parse failed" (isNothing mp))
+  
+testParseVerb s v = 
+  case parse verb "" (Utf8.fromString s) of
+    Right verb -> (v == verb)
+    Left _ -> False
+  
+testVerbs = TestList [
+  "Parse Describe" ~: testParseVerb "DESCRIBE" Describe ~=? True,
+  "Parse Announce" ~: testParseVerb "ANNOUNCE" Announce ~=? True,
+  "Parse Setup" ~: testParseVerb "SETUP" Setup ~=? True,
+  "Parse Play" ~: testParseVerb "PLAY" Play ~=? True,
+  "Parse Teardown" ~: testParseVerb "TEARDOWN" Teardown ~=? True  ]
+
+unitTests = TestList [TestLabel "Parse Minimal Request" testParseMinimalRequest,
+                      TestLabel "Packet Parsing - simple" testParsePacket,
+                      TestLabel "Packet Parsing - insuffcient data" testParsePacketNotEnoughData,
+                      testVerbs]
--- src/RtspConnection.hs
+++ src/RtspConnection.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE DeriveDataTypeable  #-}
+
 module RtspConnection(
   RtspConnection,
-  new
+  new,
+  unitTests
 ) where 
   
 import Prelude hiding (catch)
@@ -9,12 +12,13 @@ import Control.Concurrent.STM
 import Control.Exception(Exception, catch, finally, bracket, try)
 import Control.Monad
 import Control.Monad.Trans
+import qualified Data.ByteString as B
+import qualified Data.ByteString.UTF8 as Utf8
+import Data.Maybe
 import Data.Typeable
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
-import qualified Data.ByteString as B
-
-import Data.Maybe
+import Test.HUnit(Test(..), assertBool, assertEqual)
 
 import qualified Rtsp as Rtsp
 
@@ -23,10 +27,15 @@ data ConnReply = OK
 
 type ReplyVar = TMVar ConnReply
   
+data ItemToSend = Message Rtsp.Message (Maybe B.ByteString)
+                | Packet Rtsp.Packet
+
 data ConnMsg = Hello ReplyVar
              | InboundMsg Rtsp.Message (Maybe B.ByteString)
              | InboundPacket Rtsp.Packet
+             | GetItemToSend (TMVar ItemToSend)
              | BadRequest
+             | Exit
 
 type MsgQ = TChan ConnMsg
 
@@ -36,11 +45,9 @@ instance Show (TMVar a) where
   show _ = "tmvar"
 
 -- | An exception thrown into the reader thread to signal a shutdown request
-data ExitReader = ExitReader ReplyVar 
+data ExitThread = ExitThread ReplyVar
   deriving (Show, Typeable)
-instance Exception ExitReader
-
---type MaybeIO a = MaybeT IO a
+instance Exception ExitThread
 
 -- | Spawns a new connection manager thread and returns a RtspConnection that 
 --   can used to address the connection
@@ -52,15 +59,17 @@ new s maxData = do
   
 -- | Defines a state block for the connection.
 data ConnState = State {
-  readerThread :: !ThreadId
+  readerThread :: !ThreadId,
+  writerThread :: !ThreadId
 }
 
--- | The main connection thread. Spawns a reader thread and then starts handling all
---   of the messages sent to the connection.
+-- | The main connection thread. Spawns a reader thread and then starts
+--   handling all of the messages sent to the connection.
 runConnection :: Socket -> Int -> MsgQ -> IO ()
 runConnection s maxData msgQ = do
-    tid <- forkIO $ reader s maxData msgQ
-    loop s msgQ (State tid) `finally` sClose s
+    readerTid <- forkIO $ reader s maxData msgQ
+    writerTid <- forkIO $ writer s msgQ
+    loop s msgQ (State readerTid writerTid) `finally` sClose s
   where 
     loop :: Socket -> MsgQ -> ConnState -> IO ()
     loop s q state = do
@@ -74,7 +83,8 @@ runConnection s maxData msgQ = do
           state' <- processMessage msg body state
           loop s q state'
           
-        InboundPacket p -> loop s q state
+        InboundPacket p -> do 
+          loop s q state
           
         BadRequest -> do 
           -- send bad request response and bail
@@ -88,16 +98,36 @@ processMessage msg body state = do
 postMessage :: ConnMsg -> MsgQ -> IO ()
 postMessage msg q = atomically $ writeTChan q msg
   
+-- | 
+writer :: Socket -> MsgQ -> IO ()
+writer s msgQ = do
+    receiver <- newEmptyTMVarIO
+    writeLoop s msgQ receiver `catch`
+      \(ExitThread rpy) -> atomically $ putTMVar rpy OK
+  where
+    writeLoop :: Socket -> MsgQ -> (TMVar ItemToSend) -> IO ()
+    writeLoop s msgQ rx = do
+      atomically $ writeTChan msgQ (GetItemToSend rx)
+      item <- atomically $ readTMVar rx
+      let bytes = format item
+      send s bytes
+      writeLoop s msgQ rx
+      
+    format :: ItemToSend -> B.ByteString
+    format (Message msg body) = Rtsp.formatMessage msg body
+    format (Packet p) = Rtsp.formatPacket p
+      
 -- | The state block for the reader thread
-data ReaderState = RS (Maybe Rtsp.Message)
+data ReaderState = RS (Maybe Rtsp.Message) Int
 
+      
 -- | The main reader loop. Reads data from the network, parses it and sends the
 --   parsed messages on to the connection manager thread
 reader :: Socket -> Int -> MsgQ -> IO ()
 reader s maxData q = do 
-    let state = RS Nothing
+    let state = RS Nothing 0
     readLoop s maxData (B.empty) q state `catch` 
-      \(ExitReader rpy) -> atomically $ putTMVar rpy OK
+      \(ExitThread rpy) -> atomically $ putTMVar rpy OK
   where 
     readLoop :: Socket -> Int -> B.ByteString -> MsgQ -> ReaderState -> IO ()
     readLoop s maxData pending q state = do
@@ -106,38 +136,77 @@ reader s maxData q = do
       (remainder, state') <- process q buffer state
       readLoop s maxData remainder q state'
       
-    process :: MsgQ -> B.ByteString -> ReaderState -> IO (B.ByteString, ReaderState)
-    process msgQ bytes state@(RS pending)
-      | B.length bytes == 0 = return (bytes, state)
-      
-      | isJust pending = do
-          let msg = fromJust pending
-          let contentLength = Rtsp.msgContentLength msg
-          case (B.length bytes) >= contentLength of 
-            False -> return (bytes, state)
-            True -> do
-              let (body, remainder) = B.splitAt contentLength bytes
-              postMessage (InboundMsg msg (Just body)) msgQ
-              let state' = RS Nothing
-              process msgQ remainder state'
-              
-      | (isNothing pending) && (B.head bytes == 0x24) = do
-           case Rtsp.embeddedPacket bytes of 
-             Just (packet, remainder) -> do 
-               postMessage (InboundPacket packet) msgQ
-               process msgQ remainder state
-             Nothing -> return (bytes, state)
-            
-      | isNothing pending = do
-          case  Rtsp.extractMessageBytes bytes of 
-            Just (msg, remainder) -> do 
-              case (Rtsp.parseMessage msg) of 
-                Nothing -> do
-                  postMessage BadRequest msgQ
-                  return (remainder, state)
+process :: MsgQ -> B.ByteString -> ReaderState -> IO (B.ByteString, ReaderState)
+process msgQ bytes state@(RS pending cLen)
+  | B.length bytes == 0 = return (bytes, state)
+  
+  | isJust pending = do
+      let msg = fromJust pending
+      let contentLength = Rtsp.msgContentLength msg
+      case (B.length bytes) >= contentLength of 
+        False -> return (bytes, state)
+        True -> do
+          let (body, remainder) = B.splitAt contentLength bytes
+          postMessage (InboundMsg msg (Just body)) msgQ
+          let state' = RS Nothing 0
+          process msgQ remainder state'
+          
+  | (isNothing pending) && (B.head bytes == 0x24) = do
+       case Rtsp.embeddedPacket bytes of 
+         Just (packet, remainder) -> do 
+           postMessage (InboundPacket packet) msgQ
+           process msgQ remainder state
+         Nothing -> return (bytes, state)
+        
+  | isNothing pending = do
+      case  Rtsp.extractMessageBytes bytes of 
+        Just (msg, remainder) -> do 
+          case (Rtsp.parseMessage msg) of 
+            Nothing -> do
+              postMessage BadRequest msgQ
+              return (remainder, state)
 
-                Just msg -> do 
-                  if Rtsp.msgContentLength msg > 0 
-                    then process msgQ remainder (RS (Just msg))
-                    else do postMessage (InboundMsg msg Nothing) msgQ
-                            process msgQ remainder state
+            Just msg -> do 
+              let cLen' = Rtsp.msgContentLength msg
+              if cLen' > 0 
+                then process msgQ remainder (RS (Just msg) cLen)
+                else do postMessage (InboundMsg msg Nothing) msgQ
+                        process msgQ remainder state
+                        
+        Nothing -> return (bytes, state)
+
+-- ----------------------------------------------------------------------------
+-- Unit tests
+-- ----------------------------------------------------------------------------
+
+testProcessing = TestCase (do
+  let bytes = B.concat [ Utf8.fromString "DESCRIBE rtsp://localhost/root/1 RTSP/1.0\r\nCSeq: 1\r\nContent-Length: 10\r\n\r\n",
+                         B.pack [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                         B.pack [0x24, 16, 00, 05, 00, 01, 02, 03, 04],
+                         Utf8.fromString "SETUP rtsp://localhost/root/1 RTSP/1.0\r\nCSeq: 2\r\n\r\n",
+                         B.pack [0x24, 17, 00, 05, 00, 01, 02, 03, 04],
+                         B.pack [0x24, 18, 00, 05, 00, 01, 02, 03, 04] ]
+    
+  q <- newTChanIO
+  
+  process q bytes (RS Nothing 0)
+  
+  m1 <- atomically $ readTChan q 
+  case m1 of 
+    InboundMsg msg (Just body) -> do
+      assertEqual "Body Length" 10 (B.length body)
+    _ -> do
+      assertBool "Expected Message with body" False
+
+  m2 <- atomically $ readTChan q 
+  case m2 of
+    InboundPacket (Rtsp.Packet ch p) -> do assertEqual "Channel" 16 ch
+                                           assertEqual "Packet Size" 5 (B.length p)
+    _ ->  assertBool "Expected Packet" False
+    
+  m3 <- atomically $ readTChan q 
+  case m3 of
+    InboundMsg msg Nothing -> return()
+    _ -> assertBool "Expected message with no body" False) 
+  
+unitTests = TestList [testProcessing]
