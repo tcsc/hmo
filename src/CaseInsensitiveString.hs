49ffd93549170d5d6df7bdefe0b327e77b5110b6
Trent Clarke <1959769+tcsc@users.noreply.github.com>
2011-04-23 02:38:14 +0000

    Some unit tests for the case insensitive string comparison type

--- src/CaseInsensitiveString.hs
+++ src/CaseInsensitiveString.hs
@@ -1,6 +1,11 @@
-module CaseInsensitiveString (CaseInsensitiveString, fromString) where
+module CaseInsensitiveString (
+  CaseInsensitiveString, 
+  fromString, 
+  unitTests
+) where
 
 import Data.Char
+import Test.HUnit
 
 -- | Wraps a standard string with a phantom type for performing case-insensitive
 --   comparisons
@@ -14,3 +19,16 @@ instance Ord CaseInsensitiveString where
   
 fromString :: String -> CaseInsensitiveString
 fromString s = CIS s
+
+-- ----------------------------------------------------------------------------
+-- Unit Tests
+-- ----------------------------------------------------------------------------
+
+unitTests = TestList [
+  "Simple Equality"    ~: True  ~=? (fromString "lower") == (fromString "lower"),
+  "Mixed Eqaulity"     ~: True  ~=? (fromString "mIxeDCAse") == (fromString "MixedCase"),
+  "Mixed Ordering LT"  ~: True  ~=? (fromString "a") <  (fromString "B"),
+  "Mixed Ordering GTE" ~: False ~=? (fromString "a") >= (fromString "B"),
+  "Mixed Ordering GT"  ~: True  ~=? (fromString "b") >  (fromString "A"),
+  "Mixed Ordering LTE" ~: False ~=? (fromString "b") <= (fromString "A")
+  ]
